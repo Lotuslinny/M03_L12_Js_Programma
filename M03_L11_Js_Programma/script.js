db6132c5f07afdb792c3f700f8d8e01bb7724aa4
Lotuslinny <67099000+Lotuslinny@users.noreply.github.com>
2020-09-02 11:56:28 +0200

    Add files via upload
    M03_L12_Js_Programma

---
+++ M03_L11_Js_Programma/script.js
@@ -0,0 +1,12 @@
+console.log("Hallo Winc Academy Studenten")
+
+// Dit is een grote som. Deze zie je niet in de uitkomst van de console, omdat door de  comment- out komt.
+/* let multiplication = 1230941 * 1823794;
+console.log('multiplication:', multiplication);
+
+let devide = 637263 / 54;
+console.log('devide:', devide);
+ */
+
+
+
